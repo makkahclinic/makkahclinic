6d58f2ec5f10cc07e879408101e0780465fa998e
makkahclinic <makkah@makkahmedicalclinic.org>
2025-07-12 22:06:54 -0400

    Update gpt.js

--- api/gpt.js
+++ api/gpt.js
@@ -4,8 +4,11 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  return res.status(200).end();
+}
 
   if (req.method !== 'POST') {
     res.setHeader('Allow', ['POST']);
